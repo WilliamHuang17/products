dcac60f31164c0a758a1862a3ef51c38f125a403
William <zizou.ee99@g2.nctu.edu.tw>
2021-09-05 14:13:45 +0800

    code

--- products_r_w.py
+++ products_r_w.py
@@ -1,20 +1,22 @@
 import random
 import os
 
+product_list = []
+
 if os.path.isfile('product.csv'):
 	print('file product.csv exists')
+	with open('product.csv', 'r') as f:
+		for p in f:
+			if 'a,1' in p:
+				continue
+			name, price =  p.strip().split(',')
+			product_list.append([name, price])		
 else:
 	print('file product.csv not exists \n')
-	print('teminated \n')
-	quit()
-# check if file exists
-product_list = []
-with open('product.csv', 'r') as f:
-	for p in f:
-		if 'a,1' in p:
-			continue
-		name, price =  p.strip().split(',')
-		product_list.append([name, price])		
+	#print('teminated \n')
+	#quit()
+
+
 while True:
 	name = input('product name ? ')
 	if name == 'q':
@@ -23,5 +25,4 @@ while True:
 	product_list.append([name, price])
 with open('product.csv', 'w')	as f:
 	for p in product_list:
-		f.write(p[0] + ',' + p[1] + '\n')
-
+		f.write(p[0] + ',' + p[1] + '\n')	
